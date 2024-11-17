3d0a0503beb8f646d93171203f28fc42ab29fbf4
Anupam Khargharia <kharghariaanupam07@gmail.com>
2024-11-16 19:23:19 -0500

    modify to remove webcam test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import TopBar from "./components/Navbar/TopBar";
 import Home from "./pages/Home";
 import Coupon from "./pages/Coupon";
-import WebcamDetection from "./components/WebCamDetection";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/scan-products" element={<WebcamDetection />} />
+        <Route path="/scan-products" element={<TopBar />} />
         <Route path="/coupon" element={<Coupon />} />
       </Routes>
     </div>
